ff9829367a869f3137d5afaea1a29e4a927c625c
Simon Gate <simon@smgt.me>
2012-06-20 14:01:43 +0200

    Add feedbooks.com as a service

--- inject.js
+++ inject.js
@@ -1,21 +1,57 @@
-
-Array.prototype.forEach.call(document.querySelectorAll("a[type='application/epub+zip']"),function(anchor) {
-  //<div class="send-to-readmill" data-download-url="http://project.com" data-display="small" ></div>
-  var el = document.createElement("div");
-  el.className = "send-to-readmill"
-  el.setAttribute('data-download-url', anchor.href);
-  el.setAttribute('data-display', 'small');
-  var container = document.createElement("div");
-  container.style.verticalAlign = "middle";
-  container.style.marginLeft = "20px";
-  container.style.display = "inline-block";
-  container.appendChild(el);
-  anchor.parentNode.appendChild(container);
-});
-
 (function() {
+
+  var Readmill = {
+
+    button: function(url, callback, size) {
+      var el = document.createElement("div");
+      el.className = "send-to-readmill"
+      el.setAttribute('data-download-url', url);
+      if(size) {
+        el.setAttribute('data-display', size);
+      } else {
+        el.setAttribute('data-display', "small");
+      }
+
+      var container = document.createElement("div");
+      if(callback) {
+        container = callback(container);
+      }
+      container.appendChild(el);
+      return container;
+    },
+
+    appendChild: function(targets, button, insertAt) {
+      Array.prototype.forEach.call(document.querySelectorAll(targets), function(el) {
+        //<div class="send-to-readmill" data-download-url="http://project.com" data-display="small" ></div>
+        el.parentNode.appendChild(button(el));
+      });
+    }
+
+  }
+
+  // Feedbooks
+  if(/^https?:\/\/.*\.feedbooks\.com\//.test(document.location.href)) {
+    Readmill.appendChild("a[href$='.epub'].ui-button", function(el) {
+      return Readmill.button(el.href, function(button) {
+        button.style.marginTop = "10px";
+        return button;
+      },"large");
+    });
+  // Project Gutenberg
+  } else if(/^https?:\/\/.*gutenberg\.org\//.test(document.location.href)) {
+    Readmill.appendChild("a[type='application/epub+zip']", function(el) {
+      return Readmill.button(el.href, function(button) {
+          button.style.verticalAlign = "middle";
+          button.style.marginLeft = "20px";
+          button.style.display = "inline-block";
+          return button;
+      });
+    });
+  }
+
   var st = document.createElement('script'); st.type = 'text/javascript'; st.async = true;
   st.src = 'https://platform.readmill.com/send.js';
   var p = document.getElementsByTagName('body')[0];
   p.appendChild(st);
+
 })();
