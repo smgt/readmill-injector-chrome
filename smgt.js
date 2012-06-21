90b8953c5e9690b86ea5cb06899f6f9519c7afef
Simon Gate <simon@smgt.me>
2012-06-21 21:38:04 +0200

    Add script to inform smgt.me/readmill-injector if extensino is installed

---
+++ smgt.js
@@ -0,0 +1,4 @@
+var isInstalledNode = document.createElement('div');
+isInstalledNode.id = 'extension-is-installed';
+document.body.appendChild(isInstalledNode);
+document.getElementById('chrome-install-button').style.display = 'none';
