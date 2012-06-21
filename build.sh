56e8d3d661bc4300bb2b78b9c7e7d0b851373112
Simon Gate <simon@smgt.me>
2012-06-21 19:34:17 +0200

    Correct icon names in build script

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 latest_tag=$(git describe --tags)
 output_file=readmill-injector-$latest_tag.zip
-zip $output_file manifest.json inject.js icon_48.png icon_128.png
+zip $output_file manifest.json inject.js icon48.png icon128.png
 
 echo $output_file built
