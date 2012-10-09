dcdcf1e25b8bf08694fd74f841c5da661ab0a402
Simon Gate <simon@smgt.me>
2012-10-09 20:12:30 +0200

    Remove old build script

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-latest_tag=$(git describe --tags)
-output_file=readmill-injector-$latest_tag.zip
-zip $output_file smgt.js manifest.json inject.js icon48.png icon128.png
-
-echo $output_file built
